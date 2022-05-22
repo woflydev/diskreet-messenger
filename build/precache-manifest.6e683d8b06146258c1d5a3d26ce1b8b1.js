5d673f9baa8773213856ed1286819cd1feea112b
laptop _woflydev_ <wolfy.coding@gmail.com>
2022-05-23 08:26:17 +1000

    add dist

---
+++ build/precache-manifest.6e683d8b06146258c1d5a3d26ce1b8b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69cae09341d7b2c4cb1b2161d11fe1f8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "59945c5c43c3bd478ca6",
+    "url": "./static/css/main.3dba0063.chunk.css"
+  },
+  {
+    "revision": "74fa662934d9fa7411ff",
+    "url": "./static/js/2.49d10d0e.chunk.js"
+  },
+  {
+    "revision": "ab7dd6881d33ae70229d857acbe44d38",
+    "url": "./static/js/2.49d10d0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10d7a6545dcc4a117f95",
+    "url": "./static/js/3.cba413cc.chunk.js"
+  },
+  {
+    "revision": "59945c5c43c3bd478ca6",
+    "url": "./static/js/main.cc7bdd54.chunk.js"
+  },
+  {
+    "revision": "368e8cd4473c334ca0da",
+    "url": "./static/js/runtime-main.ae550d55.js"
+  }
+]);
